dbb59466cdcfaea26ed0cfbcc1ba10e8512aeace
bonbon <nepunepubon@gmail.com>
2023-10-12 20:09:03 +0800

    Fixed types in API layer function

--- src/utils/APILayer.tsx
+++ src/utils/APILayer.tsx
@@ -18,7 +18,7 @@ async function createUser(userData: { email: string; password: string; password_
 }
 
 //login. save the response headers somewhere to use in the sendMessage function
-async function loginUser(userData: { email: any; password: any; }) {
+async function loginUser(userData: { email: string; password: string; }) {
     const response = await fetch(`${BASE_URL}`, {
         method: 'POST',
         headers: {
@@ -34,15 +34,15 @@ async function loginUser(userData: { email: any; password: any; }) {
 }
 
 //send message. Get header data from the login response headers
-async function sendMessage(userData: { access_token: any; client: any; uid: any; expiry: any; receiver_id: any; receiver_class: any; body: any; }) {
+async function sendMessage(userData: { access_token: string; client: string; uid: number; expiry: Date; receiver_id: string; receiver_class: any; body: any; }) {
     const response = await fetch(`${BASE_URL}messages`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'access-token': userData.access_token,
             'client': userData.client,
-            'uid': userData.uid,
-            'expiry': userData.expiry,
+            'uid': userData.uid.toString(),
+            'expiry': userData.expiry.toString(),
         },
         body: JSON.stringify({
             "receiver_id": userData.receiver_id,
@@ -55,15 +55,15 @@ async function sendMessage(userData: { access_token: any; client: any; uid: any;
 }
 
 //get messages. Get header data from the login response headers
-async function getMessages(userData: { access_token: any; client: any; uid: any; expiry: any; }) {
+async function getMessages(userData: { access_token: string; client: string; uid: number; expiry: Date; }) {
     const response = await fetch(`${BASE_URL}messages`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
             'access-token': userData.access_token,
             'client': userData.client,
-            'uid': userData.uid,
-            'expiry': userData.expiry,
+            'uid': userData.uid.toString(),
+            'expiry': userData.expiry.toString(),
         },
     });
     const data = await response.json();
@@ -71,15 +71,15 @@ async function getMessages(userData: { access_token: any; client: any; uid: any;
 }
 
 //create channel with members
-async function createChannel(userData: { access_token: any; client: any; uid: any; expiry: any; name: any; user_ids: any; }) {
+async function createChannel(userData: { access_token: string; client: string; uid: number; expiry: Date; name: string; user_ids: number; }) {
     const response = await fetch(`${BASE_URL}channels`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'access-token': userData.access_token,
             'client': userData.client,
-            'uid': userData.uid,
-            'expiry': userData.expiry,
+            'uid': userData.uid.toString(),
+            'expiry': userData.expiry.toString(),
         },
         body: JSON.stringify({
             "name": userData.name,
@@ -91,15 +91,15 @@ async function createChannel(userData: { access_token: any; client: any; uid: an
 }
 
 //get all user's channels
-async function getChannels(userData: { access_token: any; client: any; uid: any; expiry: any; }) {
+async function getChannels(userData: { access_token: string; client: string; uid:number; expiry: Date; }) {
     const response = await fetch(`${BASE_URL}channels`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
             'access-token': userData.access_token,
             'client': userData.client,
-            'uid': userData.uid,
-            'expiry': userData.expiry,
+            'uid': userData.uid.toString(),
+            'expiry': userData.expiry.toString(),
         },
     });
     const data = await response.json();
@@ -107,15 +107,15 @@ async function getChannels(userData: { access_token: any; client: any; uid: any;
 }
 
 //get channel details via channel id
-async function getChannelDetails(userData: { channel_id: any; access_token: any; client: any; uid: any; expiry: any; }) {
+async function getChannelDetails(userData: { channel_id: number; access_token: string; client: string; uid: number; expiry: Date; }) {
     const response = await fetch(`${BASE_URL}channels/${userData.channel_id}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
             'access-token': userData.access_token,
             'client': userData.client,
-            'uid': userData.uid,
-            'expiry': userData.expiry,
+            'uid': userData.uid.toString(),
+            'expiry': userData.expiry.toString(),
         }
     });
     const data = await response.json();
@@ -123,15 +123,15 @@ async function getChannelDetails(userData: { channel_id: any; access_token: any;
 }
 
 //add member to a channel
-async function addMember(userData: { channel_id: any; access_token: any; client: any; uid: any; expiry: any; id: any; member_id: any; }) {
+async function addMember(userData: { channel_id: number; access_token: string; client: string; uid: number; expiry: Date; id: number; member_id: number; }) {
     const response = await fetch(`${BASE_URL}channels/${userData.channel_id}/add_member`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'access-token': userData.access_token,
             'client': userData.client,
-            'uid': userData.uid,
-            'expiry': userData.expiry,
+            'uid': userData.uid.toString(),
+            'expiry': userData.expiry.toString(),
         },
         body: JSON.stringify({
             "id": userData.id,
