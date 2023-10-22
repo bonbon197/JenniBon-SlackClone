a1d0d1cb5461dfed093cfb4c67b339a745306d6b
jennieLin-08 <jennelyn.hernandez2578@gmail.com>
2023-10-23 02:00:56 +0800

    direct message

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,4 +1,4 @@
-'use client' ;
+'use client';
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { createUser } from '../../utils/APILayer'
@@ -7,17 +7,17 @@ import Link from "next/link"
 import { useRouter } from 'next/navigation'
 
 import {
-    MDBBtn,
-    MDBContainer,
-    MDBRow,
-    MDBCol,
-    MDBCard,
-    MDBCardBody,
-    MDBCardImage,
-    MDBInput,
-    MDBIcon,
-    MDBCheckbox
-  }
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBInput,
+  MDBIcon,
+  MDBCheckbox
+}
 
   from 'mdb-react-ui-kit';
 
@@ -29,102 +29,91 @@ const page = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [passwordConfirmation, setPasswordConfirmation] = useState('');
-  
+
   const [name, setName] = useState('');
 
 
-  const handleSubmit = async() => {
-    if(password.length < 6){
+  const handleSubmit = async () => {
+    if (password.length < 6) {
       alert('password must be alteast six characters');
-    }else{
+    } else {
       const userData = {
         email: email,
         password: password,
         password_confirmation: passwordConfirmation
+        // name : name
       }
-        const response = createUser(userData);
-        response.then(res =>{
-          if(res.status === 'error'){
-            alert(res.errors.full_messages[0]);
-          }else{
-            createLocalUser(name, res);
-            router.push("/login");
-          }
-        }).catch(err=>{
-          console.log(err)
-          alert('Please contact your system provider!')
-        });
-        
-        // try {
-        //   const response = await createUser(userData);
-        //   console.log(response);
-        //   createLocalUser(name, response);
-        // }
-        // catch(error) {
-        //   console.log(error);
-        // }
-       
+      const response = createUser(userData);
+      response.then(res => {
+
+        if (res.status === 'error') {
+          alert(res.errors.full_messages[0]);
+        } else {
+          createLocalUser(name, res);
+          router.push("/login");
+        }
+      }).catch(err => {
+        console.log(err)
+        alert('Please contact your system provider!')
+      });
+      // try {
+      //   const response = await createUser(userData);
+      //   console.log(response);
+      //   createLocalUser(name, response);
+      // }
+      // catch(error) {
+      //   console.log(error);
+      // }
     }
-
-
-   
-
   }
-
   return (
     <>
       <MDBContainer fluid>
 
-        <MDBCard className='text-black m-5' style={{borderRadius: '25px'}}>
-        <MDBCardBody>
+        <MDBCard className='text-black m-5' style={{ borderRadius: '25px' }}>
+          <MDBCardBody>
             <MDBRow>
-            <MDBCol md='10' lg='6' className='order-2 order-lg-1 d-flex flex-column align-items-center'>
-
+              <MDBCol md='10' lg='6' className='order-2 order-lg-1 d-flex flex-column align-items-center'>
                 <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
-
                 <div className="d-flex flex-row align-items-center mb-4 ">
-                <MDBIcon fas icon="user me-3" size='lg'/>
-
-                <MDBInput label='Your Name' id='form1' type='text' onChange={(e) => setName(e.target.value)} className='w-100'/>
-
+                  <MDBIcon fas icon="user me-3" size='lg' />
+                  <MDBInput label='Your Name' id='form1' type='text' onChange={(e) => setName(e.target.value)} className='w-100' />
+                </div>
+                <div className="d-flex flex-row align-items-center mb-4">
+                  <MDBIcon fas icon="envelope me-3" size='lg' />
+                  <MDBInput label='Your Email' id='email' type='email' onChange={(e) => setEmail(e.target.value)} />
+                </div>
+                <div className="d-flex flex-row align-items-center mb-4">
+                  <MDBIcon fas icon="lock me-3" size='lg' />
+                  <MDBInput label='Password' id='password' type='password' onChange={(e) => setPassword(e.target.value)} />
                 </div>
 
                 <div className="d-flex flex-row align-items-center mb-4">
-                <MDBIcon fas icon="envelope me-3" size='lg'/>
-                <MDBInput label='Your Email' id='email' type='email' onChange={(e) => setEmail(e.target.value)}/>
-                </div>
-
-                <div className="d-flex flex-row align-items-center mb-4">
-                <MDBIcon fas icon="lock me-3" size='lg'/>
-                <MDBInput label='Password' id='password' type='password' onChange={(e) => setPassword(e.target.value)} />
-                </div>
-
-                <div className="d-flex flex-row align-items-center mb-4">
-                <MDBIcon fas icon="key me-3" size='lg'/>
-                <MDBInput label='Confirm your password' id='confirmPassword' type='password' onChange={(e) => setPasswordConfirmation(e.target.value)} />
+                  <MDBIcon fas icon="key me-3" size='lg' />
+                  <MDBInput label='Confirm your password' id='confirmPassword' type='password' onChange={(e) => setPasswordConfirmation(e.target.value)} />
                 </div>
 
                 <div className='mb-4'>
-                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='I do accept the Terms and Conditions of your site.' />
+                  <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='I do accept the Terms and Conditions of your site.' />
                 </div>
 
-                <MDBBtn color='dark' className='mb-4' size='lg' onClick={ handleSubmit }>Register</MDBBtn>
+                <MDBBtn color='dark' className='mb-4' size='lg' onClick={handleSubmit}>Register</MDBBtn>
                 <div className="text-center">
-                <p>Already have an account? <a href="/login">Login</a></p>
+                  <p>Already have an account? <a href="/login">Login</a></p>
                 </div>
 
-            </MDBCol>
+              </MDBCol>
 
-            <MDBCol md='10' lg='6' className='order-1 order-lg-2 d-flex align-items-center'>
-                <MDBCardImage src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp' fluid/>
-            </MDBCol>
+              <MDBCol md='10' lg='6' className='order-1 order-lg-2 d-flex align-items-center'>
+                <MDBCardImage src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp' fluid />
+              </MDBCol>
 
             </MDBRow>
-        </MDBCardBody>
+          </MDBCardBody>
         </MDBCard>
 
-        </MDBContainer>
-      
+      </MDBContainer>
+
     </>
   )
 }
